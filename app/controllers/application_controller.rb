7f000e688a861be6da2e93a8e12650816b8e6bc2
Mark Locklear <marklocklear@gmail.com>
2014-03-04 17:09:04 -0500

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+	before_filter :authenticate
+  def authenticate
+    authenticate_or_request_with_http_basic('Administration') do |username, password|
+      username == 'admin' && password == 'abtech'
+    end
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,6 +4,7 @@ class Student < ActiveRecord::Base
 	def self.import(file)
 		Student.delete_all
 		students = Hash.new {|hsh, key| hsh[key] = Hash.new {|inner_hsh, inner_key| inner_hsh[inner_key] = [] } }
+		#builds array of students based on person_id
 		CSV.foreach(file.tempfile, :headers => true) do |row|
 			students[row["person_id"]]["first_name"] << row["first_name"]
 			students[row["person_id"]]["last_name"] << row["last_name"]
@@ -16,7 +17,8 @@ class Student < ActiveRecord::Base
 			end
 			students[row["person_id"]]["final_grades"] << row["final_grade"]
 		end
-
+		
+		#loop through each student
 		students.each do |s|
 			end_dates = s[1]['end_dates']
 			final_grades = s[1]['final_grades']
@@ -32,14 +34,15 @@ class Student < ActiveRecord::Base
 	end
 
 	#this method handles logic for making sure latest_end_dates all have final grades of W, U, F, & R
+	#also if there are multiple latest_end_dates that are the same all grades have to be W, U, F, or R
 	def self.get_valid_students(end_dates, grades)
 		valid_end_dates= Array.new
 		valid_grades = Array.new
 		max_date = end_dates.max #gets latest end date
 		end_dates.each_with_index do |ed, index| #loop through each end_date and see if it matches latest_end_date
 			if ed == max_date												#if it does we added the date and its grade to arrays
-				valid_end_dates << ed
-				valid_grades << grades[index]
+				valid_end_dates << ed #add this date to valid_end_dates array
+				valid_grades << grades[index] #add the grade associated with the end_date above to valid_grades array
 			end
 		end
 		grade_set = ['U','W','F','R'] #only show students if latest_end_date(s) are these grades
